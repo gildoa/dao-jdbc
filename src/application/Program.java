7821592279242631a22eaf0cd911e6a87cc5dc52
araujo.gildo <gildo.campos.araujo@gmail.com>
2021-09-10 20:02:54 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -13,11 +11,13 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		//Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
 		
 		
 		//aqui, meu programa acessa a interface e não a implementação
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 			
 		System.out.println(seller);
 	}
