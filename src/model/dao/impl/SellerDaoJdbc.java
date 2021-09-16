ea6fe0c879f4a83fb235157ab4bd786502ae0654
araujo.gildo <gildo.campos.araujo@gmail.com>
2021-09-16 19:19:03 -0300

    Delete implementation

--- src/model/dao/impl/SellerDaoJdbc.java
+++ src/model/dao/impl/SellerDaoJdbc.java
@@ -90,8 +90,22 @@ public class SellerDaoJdbc implements SellerDao{
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
 		
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE id = ?");
+			st.setInt(1, id);
+			int rows = st.executeUpdate();
+			
+			if(rows == 0) {
+				throw new DbException("Id não encontrado! Não houve exclusão.");
+			}
+			
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
